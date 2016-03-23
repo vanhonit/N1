55d19d7dec4563c44b862e32247271189fd2ac64
Evan Morikawa <evan@evanmorikawa.com>
2016-03-23 16:28:42 -0400

    Lint

--- internal_packages/quick-schedule/lib/calendar-button.jsx
+++ internal_packages/quick-schedule/lib/calendar-button.jsx
@@ -19,9 +19,11 @@ export default class CalendarButton extends React.Component {
   }
 
   render() {
-    return (<button className="btn btn-toolbar" onClick={this._onClick} title="Insert calendar availability…">
-      <RetinaImg url="nylas://quick-schedule/assets/icon-composer-quickschedule@2x.png" mode={RetinaImg.Mode.ContentIsMask} />
-    </button>)
+    return (
+      <button className="btn btn-toolbar" onClick={this._onClick} title="Insert calendar availability…">
+        <RetinaImg url="nylas://quick-schedule/assets/icon-composer-quickschedule@2x.png" mode={RetinaImg.Mode.ContentIsMask} />
+      </button>
+    )
   }
 
 }
