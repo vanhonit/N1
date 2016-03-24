7089c3dc9b117ffaa48ff35f3d78ef159d5a1620
Evan Morikawa <evan@evanmorikawa.com>
2016-03-24 16:50:04 -0400

    Lint fixes

--- internal_packages/N1-Scheduler/lib/new-event-card-container.jsx
+++ internal_packages/N1-Scheduler/lib/new-event-card-container.jsx
@@ -1,7 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import NewEventCard from './new-event-card'
 import {DraftStore} from 'nylas-exports';
-import _ from 'underscore'
 
 export default class NewEventCardContainer extends Component {
   static displayName = 'NewEventCardContainer';
@@ -21,52 +20,59 @@ export default class NewEventCardContainer extends Component {
 
   componentDidMount() {
     this._mounted = true;
-    this.handleProps();
+    this._loadDraft(this.props.draftClientId);
 
-    // Add native event listeners to immediately stop
-    const node = React.findDOMNode(this.refs.event_invite_container);
-    node.addEventListener("mousedown", e => e.stopPropagation());
-    node.addEventListener("mouseup", e => e.stopPropagation());
-    node.addEventListener("keydown", e => e.stopPropagation());
+    // TODO FIXME: We need to manually block the keydown event before it
+    // hits the React event system. This is because the DAMN FOCUS
+    // handlers of the composer will catch the tab and mnaully set the
+    // focus elsewhere instead of letting us naturally tab through our
+    // form fields.
+    React.findDOMNode(this).addEventListener("keydown", this._interceptTab);
   }
 
   componentWillReceiveProps(newProps) {
-    this.handleProps(newProps);
+    this._loadDraft(newProps.draftClientId);
   }
 
   componentWillUnmount() {
     this._mounted = false;
     this._unsubscribes.forEach(s => s());
+    React.findDOMNode(this).removeEventListener("keydown", this._interceptTab);
   }
 
-  handleProps(newProps = null) {
-    const props = newProps || this.props;
-    DraftStore.sessionForClientId(props.draftClientId).then(session => {
+  _interceptTab(e) {
+    e.stopPropagation()
+  }
+
+  _loadDraft(draftClientId) {
+    DraftStore.sessionForClientId(draftClientId).then(session => {
       // Only run if things are still relevant: component is mounted
       // and draftClientIds still match
-      const idIsCurrent = newProps ? true : this.props.draftClientId === session.draftClientId;
-      if (this._mounted && idIsCurrent) {
+      if (this._mounted) {
         this._session = session;
-        const unsub = session.listen( () => { this._onDraftChange(); });
+        this._unsubscribes.forEach(s => s());
+        const unsub = session.listen(this._onDraftChange);
         this._unsubscribes.push(unsub);
         this._onDraftChange();
       }
     });
   }
 
-  _onDraftChange() {
+  _onDraftChange = () => {
     const draft = this._session.draft();
     const to = draft.to || [];
     const from = draft.from || [];
     const participants = to.concat(from);
-    draft.events.forEach(event => event.participants = participants);
+    for (const event of draft.events) {
+      event.participants = participants;
+    }
     this.setState({events: [].concat(draft.events || [])});
   }
 
   _onEventChange(change, index) {
     const events = this.state.events;
     const event = events[index].clone();
-    _.extend(event, change);
+    Object.assign({}, event, change);
     events.splice(index, 1, event);
     this._session.changes.add({events});  // triggers draft change
   }
@@ -77,21 +83,23 @@ export default class NewEventCardContainer extends Component {
     this._session.changes.add({events});  // triggers draft change
   }
 
-  _renderEventEditor(event, index) {
+  _renderNewEventCard = (event, index) => {
     return (
       <NewEventCard event={event}
-                   draft={this._session.draft()}
-                   onChange={change => this._onEventChange(change, index)}
-                   onRemove={() => this._onEventRemove(index)}
-                   onParticipantsClick={() => {}} />
-    );  // TODO focus the To: field
+        draft={this._session.draft()}
+        onRemove={() => this._onEventRemove(index)}
+        onChange={(change) => this._onEventChange(change, index)}
+        onParticipantsClick={() => {}}
+      />
+    );
   }
 
   render() {
-    return (<div className="new-event-card-container"
-                 ref="event_invite_container">
-      {this.state.events.map((evt, i) => this._renderEventEditor(evt, i))}
-    </div>)
+    return (
+      <div className="new-event-card-container">
+        {this.state.events.map(this._renderNewEventCard)}
+      </div>
+    )
   }
 }
 
--- internal_packages/N1-Scheduler/lib/new-event-card.jsx
+++ internal_packages/N1-Scheduler/lib/new-event-card.jsx
@@ -1,27 +1,22 @@
-import React, {Component, PropTypes} from 'react';
-import {Utils, DatabaseStore, Calendar} from 'nylas-exports';
-import {RetinaImg} from 'nylas-component-kit'
+import React from 'react';
 import moment from 'moment'
+import {RetinaImg} from 'nylas-component-kit'
+import EventDatetimeInput from './event-datetime-input'
+import {
+  Utils,
+  Calendar,
+  AccountStore,
+  DatabaseStore} from 'nylas-exports';
 
-
-function getDateFormat(type) {
-  switch (type) {
-  case "date": return "YYYY-MM-DD";
-  case "time": return "HH:mm:ss";
-  default: return null;
-  }
-}
-
-
-export default class NewEventCard extends Component {
+export default class NewEventCard extends React.Component {
   static displayName = 'NewEventCard';
 
   static propTypes = {
-    event: PropTypes.object.isRequired,
-    draft: PropTypes.object.isRequired,
-    onChange: PropTypes.func.isRequired,
-    onRemove: PropTypes.func.isRequired,
-    onParticipantsClick: PropTypes.func.isRequired,
+    event: React.PropTypes.object.isRequired,
+    draft: React.PropTypes.object.isRequired,
+    onChange: React.PropTypes.func.isRequired,
+    onRemove: React.PropTypes.func.isRequired,
+    onParticipantsClick: React.PropTypes.func.isRequired,
   };
 
   constructor(props) {
@@ -35,22 +30,23 @@ export default class NewEventCard extends Component {
 
   componentDidMount() {
     this._mounted = true;
-    this.setStateFromProps(this.props);
+    this._loadCalendars(this.props);
   }
 
   componentWillReceiveProps(newProps) {
-    this.setStateFromProps(newProps);
+    this._loadCalendars(newProps);
   }
 
-  setStateFromProps(props) {
-    // TODO this may not be triggered if the From: address is changed, but we need to change which
-    //      calendars are displayed
-    DatabaseStore.findAll(Calendar, {
-      accountId: props.draft.accountId,
-    }).then( calendars => {
-      if (this._mounted) {
-        this.setState({calendars: calendars && calendars.filter(c => !c.readOnly)})
-      }
+  componentWillUnmount() {
+    this._mounted = false;
+  }
+
+  _loadCalendars(props) {
+    const account = AccountStore.accountForEmail(props.draft.from[0].email);
+    DatabaseStore.findAll(Calendar, {accountId: account.id})
+    .then((calendars) => {
+      if (!this._mounted || !calendars) { return }
+      this.setState({calendars: calendars.filter(c => !c.readOnly)})
     });
   }
 
@@ -78,43 +74,61 @@ export default class NewEventCard extends Component {
           {this._renderIcon("ic-eventcard-notes@2x.png")}
 
           <input type="text"
-                 name="description"
-                 placeholder="Add notes"
-                 value={description}
-                 onChange={ e => this.props.onChange({description: e.target.value}) }/>
+            name="description"
+            placeholder="Add notes"
+            value={description}
+            onChange={ e => this.props.onChange({description: e.target.value}) }
+          />
         </div>
+
         <div className="row link">
           {this._renderIcon("ic-eventcard-link@2x.png")}
           <input type="text"
-                 name="description"
-                 placeholder="Add links"
-                 value={this.props.event.link}
-                 onChange={ e => this.props.onChange({link: e.target.value}) }/>
+            name="description"
+            placeholder="Add links"
+            value={this.props.event.link}
+            onChange={ e => this.props.onChange({link: e.target.value}) }
+          />
         </div>
         <div className="row reminder">
           {this._renderIcon("ic-eventcard-reminder@2x.png")}
 
           <input type="text"
-                 name="description"
-                 placeholder="Add reminders"
-                 value={this.props.event.reminders}
-                 onChange={ e => this.props.onChange({reminders: e.target.value}) }/>
+            name="description"
+            placeholder="Add reminders"
+            value={this.props.event.reminders}
+            onChange={ e => this.props.onChange({reminders: e.target.value}) }
+          />
         </div>
       </span>
 )
   }
 
   _renderCollapsed() {
-    return (<div className="row expand" onClick={()=>{this.setState({expanded: true})}}>
-      {this._renderIcon("ic-eventcard-disclosure@2x.png")}
-      Add reminders, notes, links...
-    </div>)
+    const ic = this._renderIcon("ic-eventcard-disclosure@2x.png");
+    const onClick = () => {this.setState({expanded: true})}
+    return (
+      <div className="row expand" onClick={onClick}>
+        {ic}
+        Add reminders, notes, links...
+      </div>
+    )
   }
 
-  _renderCalendars() {
-    return this.state.calendars.map(cal =>
+  _renderCalendarPicker() {
+    if (this.state.calendars.length <= 1) {
+      return false;
+    }
+    const calOpts = this.state.calendars.map(cal =>
       <option value={cal.serverId}>{cal.name}</option>
     );
+    const onChange = (e) => {this.props.onChange({calendarId: e.target.value})}
+    return (
+      <div className="row calendar">
+        {this._renderIcon("ic-eventcard-calendar@2x.png")}
+        <select onChange={onChange}>{calOpts}</select>
+      </div>
+    )
   }
 
   render() {
@@ -123,104 +137,57 @@ export default class NewEventCard extends Component {
       title = this.props.draft.subject;
     }
 
-    return (<div className="new-event-card">
-      <div className="remove-button" onClick={()=>this.props.onRemove()}>✕</div>
-      <div className="row title">
-        {this._renderIcon("ic-eventcard-description@2x.png")}
-        <input type="text"
-               name="title"
-               placeholder="Add an event title"
-               value={title}
-               onChange={e => this.props.onChange({title: e.target.value}) }/>
+    return (
+      <div className="new-event-card">
+        <div className="remove-button" onClick={this.props.onRemove}>✕</div>
+        <div className="row title">
+          {this._renderIcon("ic-eventcard-description@2x.png")}
+          <input type="text"
+            name="title"
+            placeholder="Add an event title"
+            value={title}
+            onChange={e => this.props.onChange({title: e.target.value}) }
+          />
+        </div>
+
+        <div className="row time">
+          {this._renderIcon("ic-eventcard-time@2x.png")}
+          <span>
+            <EventDatetimeInput name="start"
+              value={this.props.event.start}
+              onChange={ date => this.props.onChange({start: date}) }
+            />
+            -
+            <EventDatetimeInput name="end"
+              reversed
+              value={this.props.event.end}
+              onChange={ date => this.props.onChange({end: date}) }
+            />
+            <span className="timezone">
+              {moment().tz(Utils.timeZone).format("z")}
+            </span>
+          </span>
+        </div>
+
+        {this._renderCalendarPicker()}
+
+        <div className="row recipients">
+          {this._renderIcon("ic-eventcard-people@2x.png")}
+          <div onClick={this.props.onParticipantsClick()}>{this._renderParticipants()}</div>
+        </div>
+
+        <div className="row location">
+          {this._renderIcon("ic-eventcard-location@2x.png")}
+          <input type="text"
+            name="location"
+            placeholder="Add a location"
+            value={this.props.event.location}
+            onChange={e => this.props.onChange({location: e.target.value}) }
+          />
+        </div>
+
+        {this.state.expanded ? this._renderExpanded() : this._renderCollapsed()}
       </div>
-
-      <div className="row time">
-        {this._renderIcon("ic-eventcard-time@2x.png")}
-        <span>
-          <EventDatetimeInput name="start"
-                              value={this.props.event.start}
-                              onChange={ date => this.props.onChange({start: date}) } />
-          -
-          <EventDatetimeInput name="end"
-                              reversed
-                              value={this.props.event.end}
-                              onChange={ date => this.props.onChange({end: date}) } />
-          <span className="timezone">{moment().tz(Utils.timeZone).format("z")}</span>
-        </span>
-      </div>
-
-      <div className="row calendar">
-        {this._renderIcon("ic-eventcard-calendar@2x.png")}
-        <select onChange={e => {this.props.onChange({calendarId: e.target.value})} }>{this._renderCalendars()}</select>
-      </div>
-
-      <div className="row recipients">
-        {this._renderIcon("ic-eventcard-people@2x.png")}
-        <div onClick={this.props.onParticipantsClick()}>{this._renderParticipants()}</div>
-      </div>
-
-      <div className="row location">
-        {this._renderIcon("ic-eventcard-location@2x.png")}
-        <input type="text"
-               name="location"
-               placeholder="Add a location"
-               value={this.props.event.location}
-               onChange={e => this.props.onChange({location: e.target.value}) }/>
-      </div>
-
-      {this.state.expanded ? this._renderExpanded() : this._renderCollapsed()}
-    </div>)
-  }
-}
-
-class EventDatetimeInput extends Component {
-  static displayName = "EventDatetimeInput";
-
-  static propTypes = {
-    value: PropTypes.number.isRequired,
-    onChange: PropTypes.func.isRequired,
-    reversed: PropTypes.bool,
-    name: PropTypes.string,
-  };
-
-  constructor(props) {
-    super(props);
-    this._datePartStrings = {time: "", date: ""};
-  }
-
-  /*
-  componentDidMount() {
-    const dateInputNode = React.findDOMNode(this.refs.date);
-    dateInputNode.addEventListener("mousedown", () => {
-      dateInputNode.querySelector("::-webkit-calendar-picker-indicator")
-    });
-  }
-  */
-
-  _onDateChange() {
-    const {date, time} = this._datePartStrings;
-    const format = `${getDateFormat("date")} ${getDateFormat("time")}`;
-    const newDate = moment.tz(`${date} ${time}`, format, Utils.timeZone).unix();
-    this.props.onChange(newDate)
-  }
-
-  _renderInput(type) {
-    const unixDate = this.props.value;
-    this._datePartStrings[type] = unixDate != null ? moment.unix(unixDate).tz(Utils.timeZone).format(getDateFormat(type)) : null;
-    return (<input type={type}
-                   ref={type}
-                   name={`${this.props.name}-${type}`}
-                   value={this._datePartStrings[type]}
-                   onChange={e => {
-                     this._datePartStrings[type] = e.target.value;
-                     this._onDateChange()
-                   }} />)
-  }
-
-  render() {
-    if (this.props.reversed) {
-      return <span className="datetime-input-container">{this._renderInput("time")} on {this._renderInput("date")}</span>;
-    }
-    return <span className="datetime-input-container">{this._renderInput("date")} at {this._renderInput("time")}</span>;
+    )
   }
 }
